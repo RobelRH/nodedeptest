2d86afe0d74803a4411c14f03a2bf069eb05cdad
RobelRH <65722317+RobelRH@users.noreply.github.com>
2022-05-29 13:54:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
     res.send("hello from server")
 })
 
+app.get('/users', (req, res) => {
+    res.json({ message: {"fname": "robel", "lname": "hailu"} })
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
